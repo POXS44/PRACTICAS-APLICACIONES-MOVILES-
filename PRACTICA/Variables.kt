5114e9b11a69df483e9eaf29ba3a147a3393c0ae
unknown <vzeledon7@gmail.com>
2020-11-25 20:13:30 -0600

    PRIMER PARTE VARIABLES PRACTICA KOTLIN

---
+++ PRACTICA/Variables.kt
@@ -0,0 +1,20 @@
+
+fun main ()
+{
+    var nombre = "su nombre"
+    nombre = "Victor"
+
+    println("mi nombre es $nombre ")
+
+    var apellido = "su apellido"
+    apellido = "Zeledon"
+    println("mi apellido es $apellido ")
+    var edad = "edad"
+    edad = "20"    
+
+    println("mi nombre es $nombre $apellido y tengo $edad años")
+
+}
+
+
+
