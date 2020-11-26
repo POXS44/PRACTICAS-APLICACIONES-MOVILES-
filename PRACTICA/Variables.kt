5f55a7aa62e5f31927dafbb752cca4dc47b6ba76
unknown <vzeledon7@gmail.com>
2020-11-26 12:31:52 -0600

    TIPOS BOOLEAN

--- PRACTICA/Variables.kt
+++ PRACTICA/Variables.kt
@@ -3,17 +3,15 @@ fun main ()
 {
     var nombre = "su nombre"
     nombre = "Victor"
-
-    println("mi nombre es $nombre ")
-
+    
     var apellido = "su apellido"
     apellido = "Zeledon"
-    println("mi apellido es $apellido ")
-    var edad = "edad"
-    edad = "20"    
+    
+    var edad = "mi edad"
+    edad= "20"
 
     println("mi nombre es $nombre $apellido y tengo $edad años")
-
+   
 }
 
 
